589068d0013773e546a5bdb0bb9e90215674b91a
sahana1307 <42033937+sahana1307@users.noreply.github.com>
2020-02-27 10:35:01 +0530

    Add files via upload

---
+++ beamrsteeringv141d.m
@@ -0,0 +1,40 @@
+%2D FFT beamforming
+clc;
+clear all;
+close all;
+c=3e8;
+%frequency and wavelength of observing source
+f=1420e6;
+lambda=c/f;
+ls = 2;
+%distance between antennas in x direction
+dx = lambda/ls;
+%propagation constant
+k = 2*pi/lambda;
+%Number of antennas in y direction
+M = 8;
+%zenithangle
+zenithangle = 0:1:90;
+zenithangler = deg2rad(zenithangle);
+A = ones(1,M);
+
+y = 1;
+
+  %Source direction
+sourcedirdze = 90;
+sourcedirzer = deg2rad(sourcedirdze);
+m = 1:1:M;
+%Progressive Phase shift in y and x direction 
+delta = -k.*dx.*cos(sourcedirzer);
+
+ for p =  1:length(zenithangler) 
+ w2 = A(m).*exp(1i.*(m-1).*(k.*dx.*cos(zenithangler(p))+delta)); 
+ AF(p) = sum(w2);
+endfor
+
+plot(zenithangle,AF);
+ [maxP,maxIdx]=max(AF);
+ fprintf('Maximum value %f at theta=%f\n',maxP,zenithangle(maxIdx));
+ylabel('Array factor of x');
+xlabel('zenith- -90 to 90,az- 0 and 180');
+title('Array factor')
